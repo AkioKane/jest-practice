4eebce9b517f9ea138f412f67236b21d7fd519e0
Akio <aakocherga@mail.ru>
2024-09-26 19:56:54 +0700

    Added Calculator, caesarCipher, analyzeArray and their tests

--- app.js
+++ app.js
@@ -12,7 +12,102 @@ function reverseString(string) {
   return result;
 }
 
+class Calculator {
+  constructor() {}
+
+  add(a, b) {
+    return a + b;
+  }
+
+  subtract(a, b) {
+    return a - b;
+  }
+
+  divide(a, b) {
+    return a / b;
+  }
+
+  multiply(a, b) {
+    return a * b;
+  }
+}
+
+function caesarCipher(string, factor) {
+  let alphabetLower = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 
+    'h', 'i', 'j', 'k', 'l', 'm', 'n', 
+    'o', 'p', 'q', 'r', 's', 't', 'u', 
+    'v', 'w', 'x', 'y', 'z'];
+  
+  let alphabetUpper = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 
+    'H', 'I', 'J', 'K', 'L', 'M', 'N', 
+    'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
+    'V', 'W', 'X', 'Y', 'Z'];
+
+  const symbols = [
+    '?', '.', ',', '!', ';', ':', 
+    '-', '_', '+', '=', '@', '#', 
+    '$', '%', '^', '&', '*', 
+    '(', ')', '{', '}', '[', ']', 
+    '<', '>', '/', '\\', '|', 
+    '~', '`', '"', "'", " "
+  ];
+    
+
+
+  let result = "";
+  
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === symbols[symbols.indexOf(string[i])]) {
+      result += string[i];
+    } else if (string[i] === string.charAt(i).toUpperCase()) {
+      if (alphabetUpper.indexOf(string[i]) + factor >= alphabetUpper.length) {
+        let index = (alphabetUpper.indexOf(string[i]) + factor) - alphabetUpper.length;
+        result += alphabetUpper[index];
+      } else {
+        result += alphabetUpper[alphabetUpper.indexOf(string[i]) + factor];
+      }
+    } else {
+      if (alphabetLower.indexOf(string[i]) + factor >= alphabetLower.length) {
+        let index = (alphabetLower.indexOf(string[i]) + factor) - alphabetLower.length;
+        result += alphabetLower[index];
+      } else {
+        result += alphabetLower[alphabetLower.indexOf(string[i]) + factor];
+      }
+    }
+  }
+  
+  return result;
+}
+
+function analyzeArray(array) {
+  const result = {
+    average: null,
+    min: array[0],
+    max: array[0],
+    length: array.length
+  };
+
+  for (let i = 0; i <=array.length; i++) {
+    if (result.average === null) {
+      result.average = array.reduce((acc, value) => acc + value) / array.length;
+    }
+
+    if (result.min > array[i]) {
+      result.min = array[i];
+    }
+    
+    if (result.max < array[i]) {
+      result.max = array[i];
+    }
+  }
+
+  return result;
+}
+
 export {
   capitalize,
-  reverseString
+  reverseString,
+  caesarCipher,
+  analyzeArray,
+  Calculator
 }
--- app.test.js
+++ app.test.js
@@ -1,4 +1,4 @@
-import { capitalize, reverseString } from "./app";
+import { capitalize, reverseString, Calculator, caesarCipher, analyzeArray} from "./app";
 
 test('test capitalize function', () => {
   expect(capitalize("marie")).toBe("Marie");
@@ -7,3 +7,26 @@ test('test capitalize function', () => {
 test('test reverseString function', () => {
   expect(reverseString("Marie")).toBe("eiraM");
 });
+
+test('test Calculator class', () => {
+  const calc = new Calculator();
+  expect(calc.add(1, 3)).toBe(4);
+  expect(calc.subtract(3, 1)).toBe(2);
+  expect(calc.divide(4, 2)).toBe(2);
+  expect(calc.multiply(5, 5)).toBe(25);
+});
+
+test('test caesarCipher function', () => {
+  expect(caesarCipher('HeLLo', 3)).toBe("KhOOr");
+  expect(caesarCipher('xyz', 3)).toBe("abc");
+  expect(caesarCipher('Hello, World!', 3)).toBe('Khoor, Zruog!')
+});
+
+test('test analyzeArray fucntion', () => {
+  const object = analyzeArray([1,8,3,4,2,6]);
+
+  expect(object.average).toBe(4);
+  expect(object.min).toBe(1);
+  expect(object.max).toBe(8);
+  expect(object.length).toBe(6);
+})
